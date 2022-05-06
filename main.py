faba948e57887391db24a6cb4f1802e610c35601
Rebeca Domocos <rebeca.domocos@stud.ubbcluj.ro>
2022-05-06 13:22:29 +0300

    added data base

--- main.py
+++ main.py
@@ -11,17 +11,17 @@ def SpeakText(command):
 
 def main():
     r = sr.Recognizer()
-    with sr.Microphone() as source2:
-        r.adjust_for_ambient_noise(source2, duration=0.2)
-        print("say smth")
-        # listens for the user's input
-        audio2 = r.listen(source2, phrase_time_limit=2)
-
-        # Using google to recognize audio
-        MyText = r.recognize_google(audio2)
-        #MyText = MyText.lower()
-
-        print("Did you say " + MyText)
+    # with sr.Microphone() as source2:
+    #     r.adjust_for_ambient_noise(source2, duration=0.2)
+    #     print("say smth")
+    #     # listens for the user's input
+    #     audio2 = r.listen(source2, phrase_time_limit=2)
+    #
+    #     # Using google to recognize audio
+    #     MyText = r.recognize_google(audio2)
+    #     #MyText = MyText.lower()
+    #
+    #     print("Did you say " + MyText)
         # r.adjust_for_ambient_noise(source, duration=0.2)
         #
         # print("Say something...")
@@ -32,12 +32,12 @@ def main():
         #
         # except Exception as e:
         #     print("Error" + str(e))
-    filename = './dataset1/1272-141231-0000.flac'
+    filename = 'dataset2/Recording11.flac'
     with sr.AudioFile(filename) as source:
         # listen for the data (load audio to memory)
         audio_data = r.record(source)
         # recognize (convert from speech to text)
-        text = r.recognize_google(audio_data)
+        text = r.recognize_google(audio_data, language='ro')
         print(text)
     # while (1):
     #
